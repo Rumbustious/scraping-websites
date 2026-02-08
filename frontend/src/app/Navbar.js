aa3e50ecda43c410f0f3a77e1477538d6e2c6f73
Ahmed Shazly <rumbustious0@gmail.com>
2026-02-08 07:12:13 +0200

    Remove logo image from Navbar
    Removed logo image from the Navbar component.

--- frontend/src/app/Navbar.js
+++ frontend/src/app/Navbar.js
@@ -6,14 +6,6 @@ const Navbar = () => {
   return (
     <nav className="bg-green-600 bg-opacity-75 p-4 flex justify-between items-center">
       <div className="flex items-center">
-        <Image
-          src="/logo.jpg"
-          alt="Logo"
-          width={100}
-          height={40}
-          className="mr-4"
-        />
-      </div>
       <div className="text-white text-xl font-bold justify-center">
         Store Scraper
       </div>
