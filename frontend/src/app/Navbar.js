56da06a46098a7562b81f4d6fbf7dfd1516040ad
Ahmed Shazly <rumbustious0@gmail.com>
2026-02-08 07:14:28 +0200

    Refactor Navbar component structure
    Removed unnecessary div wrapper in Navbar component.

--- frontend/src/app/Navbar.js
+++ frontend/src/app/Navbar.js
@@ -5,7 +5,6 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="bg-green-600 bg-opacity-75 p-4 flex justify-between items-center">
-      <div className="flex items-center">
       <div className="text-white text-xl font-bold justify-center">
         Store Scraper
       </div>
